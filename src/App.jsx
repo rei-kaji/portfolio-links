be21d07e6b5d8cfc3b2b0df00191ce11064a50e2
rei-kaji <100171821+rei-kaji@users.noreply.github.com>
2025-12-29 09:25:40 +0900

    Update linktree

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import LinkTree from "./components/LinkTree";
 
 const App = () => {
   const links = [
-    {
-      name: "Resume",
-      url: "https://drive.google.com/drive/folders/1JsL-7dREExO6D8xwjgOFriz2QVMrHFMi?usp=sharing",
-    },
+    // {
+    //   name: "Resume",
+    //   url: "https://drive.google.com/drive/folders/1JsL-7dREExO6D8xwjgOFriz2QVMrHFMi?usp=sharing",
+    // },
     {
       name: "Portfolio 2D",
       url: "https://rei-portfolio-2d.netlify.app/",
@@ -22,15 +22,15 @@ const App = () => {
     {
       name: "LinkedIn",
       url: "https://www.linkedin.com/in/reikaji",
-    },
-    {
-      name: "Instagram",
-      url: "https://www.instagram.com/rei_kaji0/",
-    },
-    {
-      name: "Facebook",
-      url: "https://www.facebook.com/rei.kajiwara.9?mibextid=LQQJ4d",
-    },
+    // },
+    // {
+    //   name: "Instagram",
+    //   url: "https://www.instagram.com/rei_kaji0/",
+    // },
+    // {
+    //   name: "Facebook",
+    //   url: "https://www.facebook.com/rei.kajiwara.9?mibextid=LQQJ4d",
+    // },
   ];
 
   return <LinkTree links={links} className="relative z-10" />;
